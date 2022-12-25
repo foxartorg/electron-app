c42ad3ca3eb48cb5bfee7bfde9df94100fc6a1ca
Ivan Kosenko <foxartorg@gmail.com>
2022-12-25 18:32:42 +0200

    fix

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+// import {Component, OnInit} from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   title = 'electron-app';
+// }
+// import {ElectronService} from 'ngx-electron';
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {ElectronService} from "ngx-electron";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'electron-app';
+  sources = [];
+  selectedSource: any;
+  videostream: any;
+  @ViewChild('videoElement', {static: true}) videoElement: any;
+  video: any;
+
+  constructor(private _electronService: ElectronService) {
+  }
+
+  ngOnInit(): void {
+    this.video = this.videoElement.nativeElement;
+  }
+
+  displaySources() {
+    // if (this._electronService.isElectronApp) {
+    //   this._electronService.desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+    //     if (error) throw error;
+    //     this.sources = sources;
+    //     if (this.sources.length > 0) {
+    //       this.selectedSource = this.sources[0];
+    //     }
+    //   });
+    // }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import {AppComponent} from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgxElectronModule} from 'ngx-electron';
+import {AppRoutingModule} from './app-routing.module';
+
+// [...]
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxElectronModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
