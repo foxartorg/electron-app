361a32f4c3e531333c3edd8b142de239e6357e53
Ivan Kosenko <foxartorg@gmail.com>
2022-12-26 13:14:38 +0200

    fix

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const {app, BrowserWindow} = require('electron')
-const url = require("url");
-const path = require("path");
-let mainWindow
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800, height: 600, webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, `/dist/electron-app/index.html`), protocol: "file:", slashes: true
-  }));
+    pathname: path.join(__dirname, `/dist/electron-app/index.html`), protocol: 'file:', slashes: true,
+  })).then(() => {
+    mainWindow.webContents.openDevTools();
+    console.log('XXX');
+  });
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-app.on('activate', function () {
-  if (mainWindow === null) createWindow()
-})
+app.on('ready', createWindow);
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', function() {
+  if (mainWindow === null) createWindow();
+});
